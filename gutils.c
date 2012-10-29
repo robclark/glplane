728d22e93df64a63e74502d66035964ffeecd4a0
Ville Syrjälä <ville.syrjala@linux.intel.com>
2012-10-29 17:27:49 +0200

    add surface stuff

--- gutils.c
+++ gutils.c
@@ -1,5 +1,17 @@
+/*
+ *
+ */
 
-struct buffer *find_buffer_by_fb_id(struct surface *s, uint32_t fb_id)
+#include "gutils.h"
+
+int surface_get_free_buffer(struct base_surface *s)
+{
+	if (gbm_surface_has_free_buffers(s->gbm_surface))
+		return 0;
+	return -1;
+}
+
+struct base_buffer *surface_find_buffer_by_fb_id(struct base_surface *s, uint32_t fb_id)
 {
 	int i;
 
@@ -13,28 +25,28 @@ struct buffer *find_buffer_by_fb_id(struct surface *s, uint32_t fb_id)
 	return NULL;
 }
 
-void buffer_put_fb(int fd, struct surface *s, struct buffer *b)
+void surface_buffer_put_fb(int fd, struct base_surface *s, struct base_buffer *b)
 {
 	b->ref--;
-	gbm_surface_release_buffer(s->gs, b->bo);
+	gbm_surface_release_buffer(s->gbm_surface, b->bo);
 }
 
-void buffer_nuke(struct gbm_bo *bo, void *data)
+static void buffer_nuke(struct gbm_bo *bo, void *data)
 {
-	struct buffer *b = data;
+	struct base_buffer *b = data;
 
 	assert(b->ref == 0);
 
 	drmModeRmFB(b->fd, b->fb_id);
 }
 
-struct buffer *surface_get_front(int fd, struct surface *s)
+struct base_buffer *surface_get_front(int fd, struct base_surface *s)
 {
 	struct gbm_bo *bo;
-	struct buffer *b;
+	struct base_buffer *b;
 	int i;
 
-	bo = gbm_surface_lock_front_buffer(s->gs);
+	bo = gbm_surface_lock_front_buffer(s->gbm_surface);
 	if (!bo)
 		return NULL;
 
@@ -54,7 +66,7 @@ struct buffer *surface_get_front(int fd, struct surface *s)
 	}
 
 	if (!b) {
-		gbm_surface_release_buffer(s->gs, bo);
+		gbm_surface_release_buffer(s->gbm_surface, bo);
 		return NULL;
 	}
 
@@ -63,12 +75,12 @@ struct buffer *surface_get_front(int fd, struct surface *s)
 	b->fd = fd;
 	b->ref = 1;
 	b->bo = bo;
-	b->handle[0] = gbm_bo_get_handle(b->bo).u32;
-	b->stride[0] = gbm_bo_get_stride(b->bo);
+	b->handle[0] = gbm_bo_get_handle(bo).u32;
+	b->stride[0] = gbm_bo_get_stride(bo);
 	b->size = b->stride[0] * s->height;
 
 	if (drmModeAddFB2(fd, s->width, s->height, s->fmt, b->handle, b->stride, b->offset, &b->fb_id, 0)) {
-		gbm_surface_release_buffer(s->gs, bo);
+		gbm_surface_release_buffer(s->gbm_surface, bo);
 		b->ref = 0;
 		return NULL;
 	}
@@ -78,3 +90,58 @@ struct buffer *surface_get_front(int fd, struct surface *s)
 	return b;
 }
 
+void surface_free(struct base_surface *s)
+{
+	if (!s->gbm_surface)
+		return;
+
+	gbm_surface_destroy(s->gbm_surface);
+	memset(s, 0, sizeof *s);
+}
+
+bool surface_alloc(int fd,
+		   EGLDisplay *dpy,
+		   struct gbm_device *gbm,
+		   struct base_surface *s,
+		   unsigned int fmt,
+		   unsigned int width,
+		   unsigned int height)
+{
+	EGLint num_configs = 0;
+	uint32_t gbm_fmt;
+	EGLConfig config;
+
+	switch (fmt) {
+	case DRM_FORMAT_XRGB8888:
+		gbm_fmt = GBM_FORMAT_XRGB8888;
+		break;
+	default:
+		return 1;
+	}
+
+	memset(s, 0, sizeof *s);
+
+	s->fmt = fmt;
+	s->width = width;
+	s->height = height;
+
+	if (!eglChooseConfig(dpy, attribs, &config, 1, &num_configs) || num_configs != 1) {
+		memset(s, 0, sizeof *s);
+		return false;
+	}
+
+	s->gbm_surface = gbm_surface_create(gbm, width, height, gbm_fmt, GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING);
+	if (!s->gbm_surface) {
+		memset(s, 0, sizeof *s);
+		return false;
+	}
+
+	s->egl_surface = eglCreateWindowSurface(dpy, config, s->gbm_surface, NULL);
+	if (s->egl_surface == EGL_NO_SURFACE) {
+		gbm_surface_destroy(s->gbm_surface);
+		memset(s, 0, sizeof *s);
+		return false;
+	}
+
+	return true;
+}
---
+++ gutils.h
@@ -0,0 +1,49 @@
+/*
+ *
+ */
+
+#ifndef GUTILS_H
+#define GUTILS_H
+
+struct base_buffer {
+	int fd;
+	unsigned int size;
+	uint32_t offset[4];
+	uint32_t stride[4];
+	uint32_t handle[4];
+	unsigned int width;
+	unsigned int height;
+	uint32_t fmt;
+	uint32_t fb_id;
+	int ref;
+	struct gbm_bo *bo;
+};
+
+struct base_surface {
+	struct gbm_surface *gbm_surface;
+	struct base_buffer buffers[8];
+	unsigned int width;
+	unsigned int height;
+	uint32_t fmt;
+	EGLSurface surface;
+};
+
+int surface_get_free_buffer(struct base_surface *s);
+
+struct base_buffer *surface_find_buffer_by_fb_id(struct base_surface *s, uint32_t fb_id);
+
+void surface_buffer_put_fb(int fd, struct base_surface *s, struct base_buffer *b);
+
+struct base_buffer *surface_get_front(int fd, struct base_surface *s);
+
+void surface_free(struct base_surface *s);
+
+bool surface_alloc(int fd,
+		   EGLDisplay *dpy,
+		   struct gbm_device *gbm,
+		   struct base_surface *s,
+		   unsigned int fmt,
+		   unsigned int width,
+		   unsigned int height);
+
+#endif
